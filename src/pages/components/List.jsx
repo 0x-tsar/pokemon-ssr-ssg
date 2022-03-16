b99208f12466a4f441f95b5c9fadc8aa1ee96439
Gustavo <contato.gustavofernandes@hotmail.com>
2022-03-15 21:36:23 -0300

    adding nem simple features

--- src/pages/components/List.jsx
+++ src/pages/components/List.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 export const Container = styled.div`
@@ -11,22 +12,42 @@ export const Container = styled.div`
 export const Card = styled.div`
   width: 150px;
   height: 200px;
-  background-color: white;
   border-radius: 10px;
   margin: 5px;
 
   :hover {
     cursor: pointer;
-    background-color: rgba(240, 240, 240);
   }
 `;
 
 const List = ({ data }) => {
   console.log(data);
   const router = useRouter();
+  const [img, setImg] = useState("");
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const done = async () => {
+      if (Object.entries(data).length !== 0) {
+        // const detailedItem = await axios.get(
+        //   `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${data.id}.json`
+        // );
+
+        data.forEach((item) => {
+          const p = item.image.replace("images/", "");
+          const picture = `https://raw.githubusercontent.com/jherr/pokemon/main/images/${p}`;
+
+          setImages((images) => [...images, picture]);
+        });
+      }
+    };
+
+    done();
+  }, []);
 
   return (
     <Container>
+      {/* {console.log(images)} */}
       {data.map((item, i) => {
         return (
           <Card
@@ -40,7 +61,13 @@ const List = ({ data }) => {
                 "/detailed"
               );
             }}
-          ></Card>
+          >
+            {images[i] ? (
+              <img src={images[i]} alt="img" width={150} height={170} />
+            ) : (
+              <></>
+            )}
+          </Card>
         );
       })}
     </Container>
--- src/pages/detailed.jsx
+++ src/pages/detailed.jsx
@@ -1,23 +1,45 @@
 import axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Detailed = () => {
   const router = useRouter();
+  const [img, setImg] = useState("");
+  const [stats, setStats] = useState([]);
+  const [type, setType] = useState([]);
 
   useEffect(() => {
     const done = async () => {
       const data = router.query;
-      console.log(data);
 
-      const detailedItem = await axios.get(
-        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${data.id}.json`
-      );
+      // peut etre implementer ceci aprés
+      // const url = window.location.href;
 
-      const imageItem = await axios.get(
-        `https://raw.githubusercontent.com/jherr/pokemon/main/images/${data.name.toLowerCase()}.jpg`
-      );
+      if (Object.entries(data).length !== 0) {
+        const detailedItem = await axios.get(
+          `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${data.id}.json`
+        );
+
+        console.log(detailedItem.data);
+        detailedItem.data.stats.map((stat) => {
+          setStats((stats) => [...stats, stat]);
+        });
+
+        const pokemonPicture = detailedItem.data.image.replace("images/", "");
+
+        // const imageItem = await axios.get(
+        //   `https://raw.githubusercontent.com/jherr/pokemon/main/images/${pokemonPicture}`
+        // );
+
+        setImg(
+          `https://raw.githubusercontent.com/jherr/pokemon/main/images/${detailedItem.data.image.replace(
+            "images/",
+            ""
+          )}`
+        );
+      }
     };
 
     done();
@@ -28,6 +50,25 @@ const Detailed = () => {
       <Link href="/">
         <a>Go Back</a>
       </Link>
+      <br />
+      {console.log(stats)}
+      {img ? (
+        <div>
+          <img src={img} alt="img" width={150} height={170} />
+          <div>
+            {stats.map((stat, key) => {
+              return (
+                <div key={key}>
+                  {stat.name}:{stat.value}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+      {/* <Image src={img} width={100} height={100} alt="Pokemon Image"></Image> */}
     </div>
   );
 };
